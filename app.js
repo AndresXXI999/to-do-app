c1a1e0d0b050e6c366ff377bf411085859b818c4
AndresXXI999 <sanchezandresxxi@gmail.com>
2026-08-13 00:58:09 -0600

    Creat task button works now, learnt DOM manipulation and js functions

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 /*my first non AI javascript app*/
 
+function createNewTask () {
+	const node = document.createElement("div");
+
+	const nodeText = document.createTextNode("New task");
+
+	node.appendChild(nodeText);
+
+	document.getElementById("taskList").appendChild(node);
+}
 
