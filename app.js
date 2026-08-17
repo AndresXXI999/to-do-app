a9e773a4e03332268513b5f22263a1a8efbe4216
AndresXXI999 <sanchezandresxxi@gmail.com>
2026-08-16 23:36:44 -0600

    Better handling and better use of functions

--- app.js
+++ app.js
@@ -2,20 +2,25 @@
 
 function createNewTask () {
 
-	//get current id
-	const taskList = document.getElementById("taskList");	
-	const id = taskList.lastElementChild.id;
-	
-	//create new task
-	node = document.createElement("div");
-	const nodeText = document.createTextNode("New task");
+	//create new id
+	const currentId = getLastId();
+	const newId = currentId === null ? 0 : parseInt(currentId) + 1;
 
-	//we need to parse first
-	const newId = parseInt(id) + 1;
+	const node = document.createElement("div");
+	const nodeText = document.createTextNode(`Task ${newId}`)
 	node.setAttribute("id",newId);
-
 	node.appendChild(nodeText);
 
 	document.getElementById("taskList").appendChild(node);
 	console.log(newId);
+}
+
+//separate function specifically created to get the last id, nothing else.
+function getLastId(){
+	const taskList = document.getElementById("taskList");
+	if (taskList.lastElementChild === null) {
+		return null;
+	}else{
+		return taskList.lastElementChild.id;
+	}
 }
