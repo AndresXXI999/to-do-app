6402fbb597b44acde6c9bae17407ace6a5a9cd9a
AndresXXI999 <sanchezandresxxi@gmail.com>
2026-08-16 21:49:33 -0600

    More DOM manupulation, parseInt and autoincrement id

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 /*my first non AI javascript app*/
 
 function createNewTask () {
-	const node = document.createElement("div");
 
+	//get current id
+	const taskList = document.getElementById("taskList");	
+	const id = taskList.lastElementChild.id;
+	
+	//create new task
+	node = document.createElement("div");
 	const nodeText = document.createTextNode("New task");
+
+	//we need to parse first
+	const newId = parseInt(id) + 1;
+	node.setAttribute("id",newId);
 
 	node.appendChild(nodeText);
 
 	document.getElementById("taskList").appendChild(node);
+	console.log(newId);
 }
-
