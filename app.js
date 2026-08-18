ddad8d1e317e22d2da482547931759789c00ad5c
AndresXXI999 <sanchezandresxxi@gmail.com>
2026-08-18 00:57:00 -0600

    Reworked the way ids are assigned by using a global array for future manipulation.

--- app.js
+++ app.js
@@ -1,26 +1,35 @@
 /*my first non AI javascript app*/
 
+let taskArray = [];
+
 function createNewTask () {
 
 	//create new id
 	const currentId = getLastId();
 	const newId = currentId === null ? 0 : parseInt(currentId) + 1;
-
+	
 	const node = document.createElement("div");
 	const nodeText = document.createTextNode(`Task ${newId}`)
+
 	node.setAttribute("id",newId);
+	let task = {id: newId,  title: "", description: ""};
+	taskArray.push(task);
+
 	node.appendChild(nodeText);
 
 	document.getElementById("taskList").appendChild(node);
-	console.log(newId);
 }
 
 //separate function specifically created to get the last id, nothing else.
 function getLastId(){
-	const taskList = document.getElementById("taskList");
-	if (taskList.lastElementChild === null) {
+	const lastElement = taskArray.length;
+
+	if(lastElement === 0) {
+		console.log("empty array");
 		return null;
-	}else{
-		return taskList.lastElementChild.id;
+	}else {
+		console.log(`current id: ${taskArray[lastElement - 1].id}`)
+		return taskArray[lastElement - 1].id;
 	}
 }
+
